dc79448b76ed4818f953f21aef06b385ec5299f4
Brian Ross <brian.ross@students.makeschool.com>
2021-05-20 18:01:57 -0400

    test libraries imported

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,8 @@
 // contract test code will go here
+const assert = require('assert');
+const ganache = require("'ganache-cli");
+//constructor function
+const Web3 = require('web3');
+//the instance to use
+const web3 = new Web3(ganache.provider());
+
