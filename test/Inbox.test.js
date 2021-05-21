8d42d133fcc4ea3b1c921e0bec3da5a6b88cdf71
Brian Ross <brian.ross@students.makeschool.com>
2021-05-20 20:07:54 -0400

    refactor to async await

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,8 +1,48 @@
 // contract test code will go here
 const assert = require('assert');
-const ganache = require("'ganache-cli");
+const ganache = require("ganache-cli");
 //constructor function
 const Web3 = require('web3');
 //the instance to use
 const web3 = new Web3(ganache.provider());
 
+let accounts;
+
+beforeEach(async () => {
+    // Get a list of all account 
+    //using instance of web3
+    accounts = await web3.eth.getAccounts()
+       
+
+    // Use one of those accounts to deploy the contract
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(accounts)
+    });
+})
+
+
+
+
+
+
+//Basic Mocha template testing code 
+
+// class Car {
+//     park() {
+//         return 'stopped';
+//     }
+
+//     drive() {
+//         return 'vroom';
+//     }
+// }
+
+// describe('CarTest',() => {
+//     it("Should return stopped", () => {
+//         const car = new Car();
+//         assert.equal(car.park(), 'stopped')
+//     })
+// });
