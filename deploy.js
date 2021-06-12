aa0f1d5f7d0b9d6043fd629c1149357e01b1d879
Brian Ross <brian.ross@students.makeschool.com>
2021-06-12 18:41:07 -0400

    deploy code and additional tests

--- deploy.js
+++ deploy.js
@@ -1,1 +1,10 @@
 // deploy code will go here
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode} = require('./compile');
+
+const provider = new HDWalletProvider(
+    'below carbon flee black theory winter bring bonus stove today tribe luggage',
+    'https://rinkeby.infura.io/v3/4536950fcada4d6db0541b7dd3e906f2'
+);
+const web3 = new Web3(provider);
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,8 +5,10 @@ const ganache = require("ganache-cli");
 const Web3 = require('web3');
 //the instance to use
 const web3 = new Web3(ganache.provider());
+const {interface, bytecode} = require('../compile');
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
     // Get a list of all account 
@@ -15,12 +17,26 @@ beforeEach(async () => {
        
 
     // Use one of those accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['hello blockchain world'] })
+        .send( {from: accounts[0], gas: '1000000'})
+        
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(accounts)
+        assert.ok(inbox.options.address);
     });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, "hello blockchain world")
+    });
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('bye').send({from: accounts[0]});
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'bye')
+    })
 })
 
 
