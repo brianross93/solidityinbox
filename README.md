3bd65bb22c8305332e2b351c04a1403f1bab400d
Brian Ross <brian.ross@students.makeschool.com>
2021-06-12 21:37:12 -0400

    finished simple contract

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # solidityinbox
+
+# Hello World Style Blockchain Application to send a message and store it on chain. 
+
+# Deployed to Rinkeby Test network with Infura 
+
+# Deployment Transaction Hash : 0x931d217662EC616Ef9CB51A323ccC08FEa2d4966
--- deploy.js
+++ deploy.js
@@ -7,4 +7,17 @@ const provider = new HDWalletProvider(
     'below carbon flee black theory winter bring bonus stove today tribe luggage',
     'https://rinkeby.infura.io/v3/4536950fcada4d6db0541b7dd3e906f2'
 );
+//web3 instance
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+ 
+const accounts = await web3.eth.getAccounts();
+console.log("Attempting to deploy from account", accounts[0]) 
+const result = await new web3.eth.Contract(JSON.parse(interface))
+.deploy({ data: bytecode, arguments: ["Hi there"] })
+.send({gas: '1000000', from: accounts[0]});
+console.log('Contract deployed to', result.options.address)
+
+};
+deploy();
